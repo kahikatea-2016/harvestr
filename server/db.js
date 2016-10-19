e3536cd205cbba3dc9d8e08b7e2b64b0e5f307e0
Andrew Hird <andrew.james.hird@gmail.com>
2016-10-20 11:51:53 +1300

    WIP db

--- server/db.js
+++ server/db.js
@@ -13,3 +13,18 @@ function getDonors () {
 function getRecipients () {
   return knex('recipients').select()
 }
+
+function getTickets () {
+  return knex('tickets')
+  .join('recipients', 'recipients_id', '=', 'recipients.id')
+  .join('donors', 'donor_id', '=', 'donor.id')
+  .join('details', 'detail_id', '=', 'details.id' )
+  .select('details.id as detailsID')
+}
+
+function getTicket () {
+  return knex('ticket')
+  .insert({
+
+  })
+}
