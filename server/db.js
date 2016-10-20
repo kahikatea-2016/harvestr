52c359cc941bec30634fb5d1afb993f2feb8b4c0
tealiie <tealiie@live.com>
2016-10-20 19:19:53 +1300

    can getDonor and getRecipient

--- server/db.js
+++ server/db.js
@@ -1,9 +1,11 @@
-var development = require('./knexfile').development
+var development = require('../knexfile').development
 var knex = require('knex')(development)
 
 module.exports = {
   getDonors: getDonors,
-  getRecipients: getRecipients
+  getRecipients: getRecipients,
+  getDonor: getDonor,
+  getRecipient: getRecipient
 }
 
 function getDonors () {
@@ -14,17 +16,24 @@ function getRecipients () {
   return knex('recipients').select()
 }
 
-function getTickets () {
-  return knex('tickets')
-  .join('recipients', 'recipients_id', '=', 'recipients.id')
-  .join('donors', 'donor_id', '=', 'donor.id')
-  .join('details', 'detail_id', '=', 'details.id' )
-  .select('details.id as detailsID')
-}
+function getDonor (id) {
+  return knex('donors')
+    .join('details', 'donors.details_id', '=', 'details.id')
+    .where('donors.id', id)
+    .select('donors.name as name', 'details.address as address', 'details.contact_person as contact', 'details.phone as phone', 'details.notes as notes')
+  }
 
-function getTicket () {
-  return knex('ticket')
-  .insert({
+  function getRecipient (id) {
+    return knex('recipients')
+    .join('details', 'recipients.details_id', '=', 'details.id')
+    .where('recipients.id', id)
+    .select('recipients.name as name', 'details.address as address', 'details.contact_person as contact', 'details.phone as phone', 'details.notes as notes')
+  }
 
-  })
-}
+
+
+
+
+    // SELECT * FROM donors
+    // JOIN details ON donors.detail_id = details.id
+    // WHERE donors.id = 1
