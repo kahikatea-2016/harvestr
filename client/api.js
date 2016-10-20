03b0c9f2efad318e407c6f1a80ca537b6d8416a4
Andrew Hird <andrew.james.hird@gmail.com>
2016-10-20 14:58:41 +1300

    getDonors, getRecipients, getTickets, getTicket written

---
+++ client/api.js
@@ -0,0 +1,88 @@
+import request from 'superagent'
+
+const url = '/v1'
+
+export default {
+  getDonors(cb) {
+    const getUrl = `${url}/donors`
+    request.get(getUrl)
+      .end((err, res) => {
+        if (err) {
+          cb(err)
+        } else {
+          const donors = res.body.map(donors => {
+            return {
+              id: donors.id,
+              donorName: donors.donorName
+            }
+          })
+          cb(null, donors)
+        }
+      })
+  }
+
+  getRecipients(cb) {
+    const getUrl = `${url}/recipients`
+    request.get(getUrl)
+      .end((err, res) => {
+        if (err) {
+          cb(err)
+        } else {
+          const recipients = res.body.map(recipients => {
+            return {
+              id: recipients.id,
+              recipientName: recipients.recipientName
+            }
+          })
+          cb(null, recipients)
+        }
+      })
+  }
+
+  getTickets(cb) {
+    const getUrl = `${url}/tickets`
+    request.get(getUrl)
+      .end((err, res) => {
+        if (err) {
+          cb(err)
+        } else {
+          const tickets = res.body.map(tickets => {
+            return {
+              id: tickets.id,
+              name: tickets.name,
+              address: tickets.address,
+              weight: tickets.weight
+            }
+          })
+          cb(null, recipients)
+        }
+      })
+  }
+
+  getTicket(id, cb) {
+    const getUrl = `${url}/ticket/:${id}`
+    request.get(getUrl)
+    .end((err, res) => {
+      if (err) {
+        cb(err)
+      } else {
+        const ticket = res.body.map(ticket => {
+          return {
+            id: ticket.id,
+            name: ticket.name,
+            address: ticket.address,
+            person: ticket.person,
+            phone: ticket.phone,
+            expectedKg: ticket.expectedKg,
+            actualKg: ticket.actualKg,
+            notes: ticket.notes,
+            comments: ticket.comments,
+            isComplete: ticket.isComplete
+          }
+        })
+        cb(null, ticket)
+      }
+    })
+  }
+
+}
