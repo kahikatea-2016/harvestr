839b626adb843471d2dc8c7adb0c87b222102edd
Andrew Hird <andrew.james.hird@gmail.com>
2016-10-20 19:26:11 +1300

    added getDoner getRecipient routes

--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,10 @@
-var db = require('/.db')
+var db = require('./db')
 
 module.exports = {
   getDonors: getDonors,
   getRecipients: getRecipients,
-  getTickets: getTickets,
-  getTicket: getTicket,
-  addTicket: addTicket,
-  updateTicket: updateTicket
+  getDonor: getDonor,
+  getRecipient: getRecipient
 }
 
 function getDonors(req, res) {
@@ -29,61 +27,81 @@ function getRecipients(req, res) {
     })
 }
 
-function getTickets(req, res) {
-  db.getTickets()
-    .then(function (tickets) {
-      res.json(tickets)
-    })
-    .catch(function (err) {
-      res.send(err.message).status(500)
-    })
+function getDonor(req, res) {
+  var donorId = req.params.id
+  db.getDoner(donorId)
+  .then(function (donor) {
+    res.json(donor)
+  })
+  .catch(function (err) {
+    res.send(err.message).status(500)
 }
 
-function getTicket(req, res) {
-  var ticketId = Number(req.params.id)
-  if (isNaN(ticketId)) {
-    res.send('invalid id').status(404)
-  } else {
-    db.getTicket(ticketId)
-      .then(function (ticket) {
-        res.json(ticket)
-      })
-      .catch(function (err) {
-        res.send(err.message).status(500)
-      })
-  }
+function getRecipient(req, res) {
+  var recipientId = req.paramns.id
+  db.getRecipient(recipientId)
+  .then(function (recipient) {
+    res.json(recipient)
+  })
+  .catch(function (err) {
+    res.send(err.message).status(500)
 }
 
-function addTicket(req, res) {
-  var ticket = {
-    recipId: req.body.recipId,
-    donorId: req.body.donorId,
-    expectedKg: req.body.expectedKg,
-    isComplete: false
-  }
-  db.addTicket(ticket)
-    .then(function () {
-      res.json(ticket)
-    })
-    .catch(function (err) {
-      res.send(err.message).status(500)
-    })
-}
-
-function updateTicket(req, res) {
-  var ticket = {
-    recipId: req.body.recipId,
-    donorId: req.body.donorId,
-    actualKg: req.body.actualKg,
-    comments: req.body.comments,
-    isComplete: req.body.isComplete
-  }
-  db.updateTicket(ticket) {
-    .then(function () {
-        res.send(ticket)
-      })
-      .catch(function (err) {
-        res.send(err.message).status(500)
-      })
-  }
-}
+// function getTickets(req, res) {
+//   db.getTickets()
+//     .then(function (tickets) {
+//       res.json(tickets)
+//     })
+//     .catch(function (err) {
+//       res.send(err.message).status(500)
+//     })
+// }
+//
+// function getTicket(req, res) {
+//   var ticketId = Number(req.params.id)
+//   if (isNaN(ticketId)) {
+//     res.send('invalid id').status(404)
+//   } else {
+//     db.getTicket(ticketId)
+//       .then(function (ticket) {
+//         res.json(ticket)
+//       })
+//       .catch(function (err) {
+//         res.send(err.message).status(500)
+//       })
+//   }
+// }
+//
+// function addTicket(req, res) {
+//   var ticket = {
+//     recipId: req.body.recipId,
+//     donorId: req.body.donorId,
+//     expectedKg: req.body.expectedKg,
+//     isComplete: false
+//   }
+//   db.addTicket(ticket)
+//     .then(function () {
+//       res.json(ticket)
+//     })
+//     .catch(function (err) {
+//       res.send(err.message).status(500)
+//     })
+// }
+//
+// function updateTicket(req, res) {
+//   var ticket = {
+//     recipId: req.body.recipId,
+//     donorId: req.body.donorId,
+//     actualKg: req.body.actualKg,
+//     comments: req.body.comments,
+//     isComplete: req.body.isComplete
+//   }
+//   db.updateTicket(ticket) {
+//     .then(function () {
+//         res.send(ticket)
+//       })
+//       .catch(function (err) {
+//         res.send(err.message).status(500)
+//       })
+//   }
+// }
--- server/server.js
+++ server/server.js
@@ -12,10 +12,14 @@ app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('/v1/donors', routes.getDonors)
 app.get('/v1/recipients', routes.getRecipients)
-app.get('/v1/tickets', routes.getTickets)
-app.get('/v1/ticket/:id', routes.getTicket)
-app.post('/v1/ticket', routes.addTicket)
-app.put('/v1/ticket', routes.updateTicket)
+// app.get('/v1/tickets', routes.getTickets)
+// app.get('/v1/ticket/:id', routes.getTicket)
+// app.post('/v1/ticket', routes.addTicket)
+// app.put('/v1/ticket', routes.updateTicket)
+
+///////////
+app.get('/v1/donors/:id', routes.getDoner)
+app.get('/v1/recipients/:id', routes.getRecipients)
 
 app.listen(PORT, function () {
   console.log('Listening on port', PORT)
