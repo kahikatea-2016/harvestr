03308ae36ece8dddb812d9f42c19ea388baf7066
Tina <tinatrenkner@gmail.com>
2016-10-23 11:55:54 +1300

    created functional dropdown for ticket adding

---
+++ client/components/AddRecipientTicket.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import Header from './Header'
+import api from '../api'
+
+let ticket = null
+let selectRecipient = null
+let expectedKg = null
+
+export default React.createClass({
+    getInitialState() {
+      return {
+        recipients: []
+      }
+    },
+
+    handleChange(event) {
+      this.setState({
+        value: event.target.value,
+        id: event.target.id
+      })
+    },
+
+    componentDidMount() {
+      api.getRecipients(this.renderResults)
+    },
+
+    renderResults(err, donors) {
+      this.setState({recipients: recipients})
+    },
+
+    addRecipientTicket () {
+      let newTicket = {
+        recipientId: selectRecipient.value,
+        expectedKg: expectedKg.value,
+        detailId: selectRecipient.value
+      }
+      console.log(newTicket)
+      api.addTicket(newTicket)
+    },
+
+    render() {
+      return (
+        <div>
+          <Header />
+          <div className="recipientForm">
+            <h2> Create a Recipient Ticket </h2>
+            <select value={this.state.value} id={this.state.id} onChange={this.handleChange} ref={function (input) {
+                selectRecipient = input
+              }}>
+              <option value="0"> Select Recipient Name </option>
+              {this.state.recipients.map((recipient) => {
+                return (
+                  <option key={recipient.id} id={recipient.id} value={recipient.id}>
+                    {recipient.recipientName}
+                  </option>
+                )
+              })}
+            </select>
+            <br/>
+            <label htmlFor="weight"> Expected Weight: </label>
+            <br/>
+            <input name="weight" type="number" min="1" max="999" placeholder="Kilograms" ref={function (input) {
+              expectedKg = input
+            }} />
+            <br/>
+            <input className="button" type="submit" value="Submit" onClick={() => this.addRecipientTicket()}/>
+          </div>
+        </div>
+      )
+    }
+  })
