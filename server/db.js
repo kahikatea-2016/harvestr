5aa91cb11219e689fd8ad35a1cd7d67a104cf7ea
Andrew Hird <andrew.james.hird@gmail.com>
2016-10-24 12:26:21 +1300

    fixed db.js indenting

--- server/db.js
+++ server/db.js
@@ -25,33 +25,33 @@ function getRecipients () {
 
 function getDonor (id) {
   return knex('donors')
-  .join('details', 'donors.detail_id', '=', 'details.id')
-  .where('donors.id', id)
-  .select('donors.name as name', 'details.address as address', 'details.contact_person as contact', 'details.phone as phone', 'details.notes as notes', 'donors.detail_id as detailId')
+    .join('details', 'donors.detail_id', '=', 'details.id')
+    .where('donors.id', id)
+    .select('donors.name as name', 'details.address as address', 'details.contact_person as contact', 'details.phone as phone', 'details.notes as notes', 'donors.detail_id as detailId')
 }
 
 function getRecipient (id) {
   return knex('recipients')
-  .join('details', 'recipients.detail_id', '=', 'details.id')
-  .where('recipients.id', id)
-  .select('recipients.name as name', 'details.address as address', 'details.contact_person as contact', 'details.phone as phone', 'details.notes as notes', 'recipients.detail_id as detailId')
+    .join('details', 'recipients.detail_id', '=', 'details.id')
+    .where('recipients.id', id)
+    .select('recipients.name as name', 'details.address as address', 'details.contact_person as contact', 'details.phone as phone', 'details.notes as notes', 'recipients.detail_id as detailId')
 }
 
 function updateTicket (ticket) {
   return knex ('tickets')
- .where('tickets.id', ticket.id)
- .update({
-   actual_kg: ticket.actualKg,
-   is_complete: true
- })
+   .where('tickets.id', ticket.id)
+   .update({
+     actual_kg: ticket.actualKg,
+     is_complete: true
+   })
 }
 
 function updateComment (comment) {
   return knex ('comments')
-  .insert({
-    ticket_id: comment.ticketId,
-    comments: comment.comment
-  })
+    .insert({
+      ticket_id: comment.ticketId,
+      comments: comment.comment
+    })
 }
 
 function getTickets () {
@@ -72,22 +72,22 @@ function addTicket (ticket) {
 
 function getDonorTicket (ticketId) {
   return knex ('tickets')
- .join ('donors', 'tickets.donor_id', '=', 'donors.id')
- .join ('details', 'tickets.details_id', '=', 'details.id')
- .where ('tickets.id', ticketId)
- .select ('tickets.id as id', 'tickets.expected_kg as expected', 'tickets.actual_kg as actual', 'donors.name as name', 'details.address as address', 'details.contact_person as contact', 'details.phone as phone', 'details.notes as notes', 'tickets.is_complete as isComplete')
+   .join ('donors', 'tickets.donor_id', '=', 'donors.id')
+   .join ('details', 'tickets.details_id', '=', 'details.id')
+   .where ('tickets.id', ticketId)
+   .select ('tickets.id as id', 'tickets.expected_kg as expected', 'tickets.actual_kg as actual', 'donors.name as name', 'details.address as address', 'details.contact_person as contact', 'details.phone as phone', 'details.notes as notes', 'tickets.is_complete as isComplete')
 }
 
 function getRecipientTicket (ticketId) {
   return knex ('tickets')
-  .join ('recipients', 'tickets.recipient_id', '=', 'recipients.id')
-  .join ('details', 'tickets.details_id', '=', 'details.id')
-  .where ('tickets.id', ticketId)
-  .select ('tickets.id as id', 'tickets.expected_kg as expected', 'tickets.actual_kg as actual', 'recipients.name as name', 'details.address as address', 'details.contact_person as contact', 'details.phone as phone', 'details.notes as notes', 'tickets.is_complete as isComplete')
+    .join ('recipients', 'tickets.recipient_id', '=', 'recipients.id')
+    .join ('details', 'tickets.details_id', '=', 'details.id')
+    .where ('tickets.id', ticketId)
+    .select ('tickets.id as id', 'tickets.expected_kg as expected', 'tickets.actual_kg as actual', 'recipients.name as name', 'details.address as address', 'details.contact_person as contact', 'details.phone as phone', 'details.notes as notes', 'tickets.is_complete as isComplete')
 }
 
 function getTicketComments (ticketId) {
   return knex ('comments')
-  .where ('ticket_id', ticketId)
-  .select()
+    .where ('ticket_id', ticketId)
+    .select()
 }
