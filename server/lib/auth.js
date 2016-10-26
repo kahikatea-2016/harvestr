9354b865ffb1d1a4252733bd3fcb9a5d6e12df6f
tealiie <tealiie@live.com>
2016-10-26 16:50:41 +1300

    can login

--- server/lib/auth.js
+++ server/lib/auth.js
@@ -8,7 +8,7 @@ const users = require('./users')
 const googleConfig = {
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3000/auth/google/callback'
+  callbackURL: '/auth/google/callback'
 }
 
 function createToken (user, secret) {
